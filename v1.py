18a99326b10f4fa0b8f0fce7e620870e257daa40
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-04-27 23:40:13 +0530

    Update v1.py

--- v1.py
+++ v1.py
@@ -147,13 +147,11 @@ def transcribe(audio_file):
 
     transcript_output_response = requests.get(endpoint, headers=headers)
     
-    '''
     # Check if transcription is complete
     while transcript_output_response.json()['status'] != 'completed':
-        sleep(5)
+        time.sleep(5)
         # print('Transcription is processing ...')
         transcript_output_response = requests.get(endpoint, headers=headers)
-    '''
         
     # Print transcribed text
     st.write(transcript_output_response.json()["text"])
