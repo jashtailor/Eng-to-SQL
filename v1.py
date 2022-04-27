c28ff7033082db232c7b7dbe04cba1d0c2e7b612
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2022-04-27 22:50:44 +0530

    Update v1.py

--- v1.py
+++ v1.py
@@ -104,6 +104,8 @@ English-To-SQL
 ''')
 
 # input = st.text_input('Enter your question in English')
+url = "https://online-voice-recorder.com/"
+st.write("You can use this link to record an audio [link](%s)" % url)
 uploaded_files = st.file_uploader("Choose a .mp3 file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
      bytes_data = uploaded_file.read()
